ff67a14241e59ed217bd062dadbc124ff2b65cb5
TimmyGOGO <temich941@mail.ru>
2019-05-24 11:47:29 +0300

    develop: Layout draft - creating components, using <style jsx>{``}</style>

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from './../src/components/Layout'
+
+const Index =() => (
+  <Layout>
+    <div>
+      <h1> Welcome to BitzPrices</h1>
+    </div>
+  </Layout>
+)
+
+export default Index
---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import Navbar from './Navbar'
+import Head from 'next/head'
+
+const Layout = (props) => (
+  <div>
+    <Head>
+      <title>BitzPrice</title>
+      <link 
+        rel="stylesheet"
+        href="https://bootswatch.com/4/cerulean/bootstrap.min.css" 
+      />
+    </Head>
+    <Navbar />
+    {props.children}
+  </div>
+)
+
+export default Layout
